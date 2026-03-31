b5d6ca3524df2dd945866f2688efe660f2b3d6c3
Becca4375 <r.adhiamboo@alustudent.com>
2026-03-31 11:37:57 -0700

    analyzed output from the raw data and was combined in one file named combined_results

---
+++ raw_data/extracted-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+(echo "The top 10 highest temperatures"
+echo ""
+tail -n +2 satelite_temperature_data.csv | cut -d ',' -f 3 | sort -nr | head -n 10
+echo ""
+echo "Results grouped according to humidity amount in descending order for Botswana"
+echo ""
+head -n 1 satelite_temperature_data.csv && grep "Botswana" satelite_temperature_data.csv | sort -t',' -k4 -rn) > combined_results
